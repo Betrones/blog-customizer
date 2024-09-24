9f8baf5baa7a02569949f1322a6ed71b7594915d
Betrones <betronesdorg@gmail.com>
2024-09-24 14:56:45 +0300

    first
    idk

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,42 @@
 import arrow from 'src/images/arrow.svg';
 
 import styles from './ArrowButton.module.scss';
+import React, { useEffect, useRef } from 'react';
+// import * as events from 'node:events';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+type TProps = {
+	isOpened: boolean;
+};
+export const ArrowButton = ({ isOpened }: TProps) => {
+	const btnRef = useRef<HTMLDivElement | null>(null);
+	const arrowRef = useRef<HTMLImageElement | null>(null);
+	useEffect(() => {
+		if (isOpened) {
+			btnRef.current?.classList.add(styles.container_open);
+			arrowRef.current?.classList.add(styles.arrow_open);
+		} else {
+			btnRef.current?.classList.remove(styles.container_open);
+			arrowRef.current?.classList.remove(styles.arrow_open);
+		}
+	}, [isOpened]);
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={styles.container}
+			onClick={() => console.log()}
+			ref={btnRef}>
+			<img
+				src={arrow}
+				ref={arrowRef}
+				alt='иконка стрелочки'
+				className={styles.arrow}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,25 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import React, { useEffect, useRef, useState } from 'react';
 
 export const ArticleParamsForm = () => {
+	const [opened, setOpened] = useState(false);
+	const asideRef = useRef<HTMLDivElement | null>(null);
+	useEffect(() => {
+		setOpened(true);
+	}, []);
+	useEffect(() => {
+		if (!opened) {
+			asideRef.current?.classList.remove(styles.container_open);
+		} else {
+			asideRef.current?.classList.add(styles.container_open);
+		}
+	}, [opened]);
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton isOpened={opened} />
+			<aside className={styles.container} ref={asideRef}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
